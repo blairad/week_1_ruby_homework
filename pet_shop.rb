504f451a6857b012a821bc5be6f670d57b7b98b9
Andrew Blair <blair.d.andrew@gmail.com>
2019-04-05 15:00:04 +0100

    start of third test

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,6 +7,15 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-# def add_or_remove_cash__add_to_total
-#
+def add_or_remove_cash__add
+  
+end
+
+
+# def total_money(people)
+#   total_money = 0
+#   for person in people
+#     total_money += person[:monies]
+#   end
+#   return total_money
 # end
