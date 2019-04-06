56e6e9b9f3f23d74205557a984a8dfdb8ea2f96e
Andrew Blair <blair.d.andrew@gmail.com>
2019-04-06 12:07:53 +0100

    breed 2 wrong. finding pet by name lost in loops

--- pet_shop.rb
+++ pet_shop.rb
@@ -26,13 +26,29 @@ def stock_count(pet_shop)
   return pet_shop[:pets].count
 end
 
-def all_pets_by_breed__found(pet_shop, breed)
-  breed_of_pet = []
-    for pet in pet_shop[:pets]
-      if pet[:breed] == "British Shorthair"
-      end
-    end
-  breed_of_pet.push[pet]
-end
+# def all_pets_by_breed__found(pet_shop, breed)
+#   breed_of_pet = []
+#     for pet in pet_shop[:pets]
+#       if pet[:breed] == "British Shorthair"
+#         breed_of_pet.push(pet)
+#       end
+#     end
+#     return breed_of_pet
+# end
+#
+#
+# # def all_pets_by_breed__not_found(pet_shop, breed)
+# #   breed_of_pet[]
+# #   for pet in pet_shop[:pets]
+# #     if pet[:breed] == "Dalmation"
+# #       return nil
+# #     end
+# #    end
+# # end
 
-#return breed_of_pet[]
+def find_and_return_pet_name(pet_by_name, name)
+  pet_name = []
+  for pet_by_name in pet_shop
+    for
+  end
+end
